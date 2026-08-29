c0a0c1c0ea4eed1f76a9f2c27a02b9a20cf6f5b9
djaoutfouad <djaoutfouad19762321@gmail.com>
2026-08-29 11:17:36 +0100

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -17,7 +17,7 @@ export const SITE_CONFIG = {
   emailjs: {
     serviceId: 'service_qfoy2xa',
     templateId: 'template_rza5fvr',
-    publicKey: 'EKAHPvwVCQPeT4Szb',
+    publicKey: 'EKAHPwVCQPeT4Szb',
   },
 };
 
